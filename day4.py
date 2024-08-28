bab2f1e38f5d393784869d7b2dc52666a40597d5
divyanshgoelcoder <15divyanshgoel@gmail.com>
2024-08-28 19:34:17 +0530

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,14 @@
+'''jo hum serach kar rhe hai usi se related add ajati hai (commercial applications)
+respone aur request ka circle kaise kaam karta hai
+http plays the role of vrhicle betwwen client prefernces and business companies vo log cookies khareed te hai jisme hamara saara data store hota hai
+
+absolute path
+relative path
+prectice karo
+
+. represents current folder for eaxmple ./a1.html matlab usi folder me
+ to go to top you can use hash only
+ id assign karne ke baad linbk create karne ke liye #id laga ke uska link ba jaat ahai
+ mail id banane ke liye
+ download karne ke liye
+'''
